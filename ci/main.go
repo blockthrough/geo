e0dd130d3fa38386a424f02085cd8666d80bf114
Zhongshi Xi <xizhongshiwise@Zhongshis-MacBook-Pro.local>
2023-10-04 12:24:10 -0400

    enable dagger

--- ci/main.go
+++ ci/main.go
@@ -20,13 +20,32 @@ func main() {
 		Container().
 		From("golang:1.21-alpine3.18").
 		WithMountedDirectory("/src", src).
-		WithWorkdir("/src")
+		WithWorkdir("/src").
+		WithExec([]string{"go", "test", "-v", "-coverprofile=cov.out", "-coverpkg=./"})
 
-	contents, err := ref.WithExec([]string{"go", "test", "-v", "-coverprofile=cov.out", "-coverpkg=./"}).Stdout(ctx)
+	_, err = ref.Stdout(ctx)
 	if err != nil {
 		fmt.Printf("go test :%s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("contents:%s\n", contents)
+	ok, err := ref.File("./cov.out").Export(ctx, "./cov.out")
+	if err != nil {
+		fmt.Printf("export coverage file :%s\n", err)
+		os.Exit(1)
+	}
+
+	if !ok {
+		fmt.Printf("can not find the coverage file :%s\n", err)
+		os.Exit(1)
+	}
+
+	// contents, err := ref.WithExec([]string{"go", "test", "-v", "-coverprofile=cov.out", "-coverpkg=./"}).Stdout(ctx)
+
+	// if err != nil {
+	// 	fmt.Printf("go test :%s\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Printf("contents:%s\n", contents)
 }
